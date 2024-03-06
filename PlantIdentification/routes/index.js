10f8f69e65f1ff3d2022699ef55e9a05c754f823
Benjamin Tindal <btindal1@sheffield.ac.uk>
2024-03-06 15:58:33 +0000

    Basic layout added for plant display page

--- PlantIdentification/routes/index.js
+++ PlantIdentification/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Plant' });
 });
-
+/* GET plant page. */
+router.get('/plant', function(req, res, next) {
+  res.render('plant', { title: 'Poison Ivy - Plant' });
+});
 module.exports = router;
