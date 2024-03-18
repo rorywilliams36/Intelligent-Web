23e035bfca383b993f9e2f60848baa28e9cf107d
Benjamin Tindal <btindal1@sheffield.ac.uk>
2024-03-18 14:38:36 +0000

    Fixed bootstrap js not working properly across all files

--- PlantIdentification/app.js
+++ PlantIdentification/app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
